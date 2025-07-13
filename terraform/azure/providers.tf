8cb73265e484b834443383dce94550e2dcb6da8d
RomaPavl <103950304+RomaPavl@users.noreply.github.com>
2025-07-13 17:39:53 +0300

    Update providers.tf

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -18,8 +18,8 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  subscription_id = "2b1816ab-fcb9-4bac-b22b-4541dc84410f"
+  tenant_id       = "7631cd62-5187-4e15-8b8e-ef653e366e7a"
   client_id       = var.client_id
   client_secret   = var.client_secret
 }
